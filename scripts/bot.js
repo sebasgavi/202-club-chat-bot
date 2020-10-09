ef081462916a24bb96b93a98a7c193bbcef79c07
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-10-08 19:56:47 -0500

    my work here is done

--- scripts/bot.js
+++ scripts/bot.js
@@ -12,34 +12,32 @@ class ChatBot {
 
   async answer(text) {
 
-    if (/^hola\s?.*/.test(text)) {
-      if (/dame un random/.test(text)) {
-        const r = Math.floor(Math.random() * 99999);
-        return `Tu random es: ${r}`;
-      }
-
-      if (/^hola\s?.*/.test(text)) {
-        return 'El bot dice "Hola"';
-      }
-
-      const introRegex = /^me llamo (.+)$/;
-      if (introRegex.test(text)) {
-        const match = text.match(introRegex);
-        console.log(match)
-        return `Hola ${match[1]}, soy Bot.`;
-      }
-
-      if (/pr[oó]ximo lanzamiento/.test(text)) {
-        const obj = await this.getNextLaunch();
-        const date = new Date(obj.date_local);
-        return `El próximo lanzamiento será el día ${date.toLocaleDateString()}`;
-      }
-
-      if (/mejor lenguaje/.test(text)) {
-        return 'El bot dice: Java > JS';
-      }
-
-      return null;
+    if (/dame un random/.test(text)) {
+      const r = Math.floor(Math.random() * 99999);
+      return `Tu random es: ${r}`;
     }
+
+    if (/^hola\s?.*/.test(text)) {
+      return 'El bot dice "Hola"';
+    }
+
+    const introRegex = /^me llamo (.+)$/;
+    if (introRegex.test(text)) {
+      const match = text.match(introRegex);
+      console.log(match)
+      return `Hola ${match[1]}, soy Bot.`;
+    }
+
+    if (/pr[oó]ximo lanzamiento/.test(text)) {
+      const obj = await this.getNextLaunch();
+      const date = new Date(obj.date_local);
+      return `El próximo lanzamiento será el día ${date.toLocaleDateString()}`;
+    }
+
+    if (/mejor lenguaje/.test(text)) {
+      return 'El bot dice: JS';
+    }
+
+    return null;
   }
 }
