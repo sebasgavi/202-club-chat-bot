43afaa3fa3c084814d3e62f7f9d19035d69f02a9
AnotherPotato <joansebastian35@gmail.com>
2020-10-08 19:28:21 -0500

    Added a new response for bot.js

--- scripts/bot.js
+++ scripts/bot.js
@@ -4,29 +4,33 @@ class ChatBot {
     this.base = 'https://api.spacexdata.com/v4';
   }
 
-  async getNextLaunch () {
+  async getNextLaunch() {
     const res = await fetch(`${this.base}/launches/next`);
     const obj = await res.json();
     return obj;
   }
 
-  async answer (text) {
+  async answer(text) {
 
-    if(/^hola\s?.*/.test(text)){
+    if (/^hola\s?.*/.test(text)) {
       return 'El bot dice "Hola"';
     }
 
     const introRegex = /^me llamo (.+)$/;
-    if(introRegex.test(text)) {
+    if (introRegex.test(text)) {
       const match = text.match(introRegex);
       console.log(match)
       return `Hola ${match[1]}, soy Bot.`;
     }
 
-    if(/pr[oó]ximo lanzamiento/.test(text)){
+    if (/pr[oó]ximo lanzamiento/.test(text)) {
       const obj = await this.getNextLaunch();
       const date = new Date(obj.date_local);
       return `El próximo lanzamiento será el día ${date.toLocaleDateString()}`;
+    }
+
+    if (/mejor lenguaje/.test(text)) {
+      return 'El bot dice: Java > JS';
     }
 
     return null;
